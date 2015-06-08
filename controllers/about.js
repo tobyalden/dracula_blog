15c93741f566919dd4478f5d4f3d697f46107969
Toby Alden & James Williams <student@epicodus.com>
2015-06-08 11:04:07 -0700

    finished project, added controllers

--- controllers/about.js
+++ controllers/about.js
@@ -1,7 +1,17 @@
 Blogger.AboutController = Ember.Controller.extend({
+  isPictureShowing: false,
   actions: {
+
     showRealName: function() {
       alert("the count counts counts counts ");
+    },
+
+    showPicture: function() {
+      this.set('isPictureShowing', true);
+    },
+    hidePicture: function() {
+      this.set('isPictureShowing', false);
     }
+
   }
 });
